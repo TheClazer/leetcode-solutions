275712dbebb2cce75e35ae8cd956f28a0bdfe203
Rayyan Ahmed Shaikh <therayyn16@gmail.com>
2026-09-15 21:07:12 +0530

    [13] [Time Beats: 100.00%] [Memory Beats: 57.35%] - LeetPush

---
+++ 13-roman-to-integer/13-Roman-to-Integer.c
@@ -0,0 +1,26 @@
+int rv(char x){
+    switch(x){
+        case 'I':return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C':return 100;
+        case 'D': return 500;
+        case 'M':return 1000;
+        default:return 0;
+    }
+}
+int romanToInt(char* s) {
+    int sum = 0;
+    for(int i =0;s[i]!='\0';i++){
+        
+        
+        if(rv(s[i+1])>rv(s[i])){
+            sum=sum+rv(s[i+1])-rv(s[i]);
+            i++;
+        }
+        else{sum=sum+rv(s[i]);}
+    }
+    return sum;
+
+}
